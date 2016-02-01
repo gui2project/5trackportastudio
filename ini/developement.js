a31728c89416821e582e1be4eac4e00643ca76b0
josefflores <jose.flores.152@gmail.com>
2016-02-01 14:35:10 -0500

    moved paths to their own configuration to remove duplicates

--- ini/developement.js
+++ ini/developement.js
@@ -1,9 +1,9 @@
 /**
  *  @file   developement.js
  *
- *  The configuration file to use during development.
+ *  The configuration file to use during developement.
  *
- *  It holds absolute paths, and other settings.
+ *  It holds settings and links to the paths for the application.
  */
 
 //  Requires
@@ -14,24 +14,7 @@ var ini = {
     app:{
         mode: 'dev'
     },
-    path: {
-        angular:     path.join(global.app.root, 'node_modules', 'angular'),
-        bootstrap:   path.join(global.app.root, 'node_modules', 'bootstrap', 'dist'),
-        ctrl:        path.join(global.app.root, 'server', 'controllers'),
-        error:       path.join(global.app.root, 'server', 'controllers', 'error', 'index.js'),
-        images:      path.join(global.app.root, 'www', 'app', 'img'),
-        jquery:      path.join(global.app.root, 'node_modules', 'jquery', 'dist'),
-        jqueryui:    path.join(global.app.root, 'node_modules', 'jquery-ui'),
-        favicon:     path.join(global.app.root, 'www', 'app', 'img', 'favicon.ico' ),
-        nodeModules: path.join(global.app.root, 'node_modules'),
-        partial:     path.join(global.app.root, 'server', 'views', 'partial'),
-        private:     path.join(global.app.root, 'server'),
-        public:      path.join(global.app.root, 'www'),
-        root:        global.app.root,
-        routes:      path.join(global.app.root, 'server', 'routes', 'index'),
-        users:       path.join(global.app.root, 'server', 'routes', 'users'),
-        views:       path.join(global.app.root, 'server', 'views')
-    }
+    path: require('./paths.js')
 };
 
 // Reporting that the configuration was used
---
+++ ini/paths.js
@@ -0,0 +1,30 @@
+/**
+ *  @file   paths.js
+ *
+ *  The configuration file for absolute paths.
+ */
+
+//  Requires
+var path            = require('path');
+
+var paths = {
+    angular:     path.join(global.app.root, 'node_modules', 'angular'),
+    bootstrap:   path.join(global.app.root, 'node_modules', 'bootstrap', 'dist'),
+    ctrl:        path.join(global.app.root, 'server', 'controllers'),
+    error:       path.join(global.app.root, 'server', 'controllers', 'error', 'index.js'),
+    images:      path.join(global.app.root, 'www', 'app', 'img'),
+    jquery:      path.join(global.app.root, 'node_modules', 'jquery', 'dist'),
+    jqueryui:    path.join(global.app.root, 'node_modules', 'jquery-ui'),
+    favicon:     path.join(global.app.root, 'www', 'app', 'img', 'favicon.ico' ),
+    nodeModules: path.join(global.app.root, 'node_modules'),
+    partial:     path.join(global.app.root, 'server', 'views', 'partial'),
+    private:     path.join(global.app.root, 'server'),
+    public:      path.join(global.app.root, 'www'),
+    root:        global.app.root,
+    routes:      path.join(global.app.root, 'server', 'routes', 'index'),
+    users:       path.join(global.app.root, 'server', 'routes', 'users'),
+    views:       path.join(global.app.root, 'server', 'views')
+}
+
+/* Application paths export*/
+module.exports = paths;
