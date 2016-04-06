90389d5e4a24737298f15f0961c698f61d8e4413
Dominic Cabral <dominiccabral@Dominics-MacBook-Pro.local>
2016-04-06 00:55:42 -0400

    Meter attachment WIP

--- www/app/js/webAudioInterface.js
+++ www/app/js/webAudioInterface.js
@@ -9,7 +9,17 @@ $(function () {
         track[index].InitTrack();
     });
 
+    setInterval(
+        function(){
+    $("#track-1 > .row > .meter").MeterVolume(track[0].meter.volume*250);
+    $("#track-2 > .row > .meter").MeterVolume(track[1].meter.volume*250);
+    $("#track-3 > .row > .meter").MeterVolume(track[2].meter.volume*250);
+    $("#track-4 > .row > .meter").MeterVolume(track[3].meter.volume*250);
+    }, 1);
+
 }); //end document ready
+       
+
 
 function armTrackToggle(trackNumber) {
     console.log('armTrackToggle track:' + trackNumber);
