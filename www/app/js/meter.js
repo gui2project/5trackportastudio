d0173ba19710c75daa22cf5ab34093282c039d25
Metiri Dubstep <Metiri Dubstep>
2016-04-05 22:22:35 -0700

    Added volume meter to application

--- www/app/js/meter.js
+++ www/app/js/meter.js
@@ -6,12 +6,14 @@
 /****************************/
 
 // Set up meters
-$(document).ready(function() {
-    $('.meter').meter(12);
-});
+$(document)
+    .ready(function () {
+        $('.meter')
+            .meter(12);
+    });
 
 // Function for making LEDs
-$.fn.meter = function(NumOfLEDs) {
+$.fn.meter = function (NumOfLEDs) {
     // Save number of LEDs for later use
     var numberOfLEDs = NumOfLEDs;
 
@@ -19,81 +21,103 @@ $.fn.meter = function(NumOfLEDs) {
     var clipping = false;
 
     // Add mixer-meter class
-    $(this).addClass('mixer-meter');
+    $(this)
+        .addClass('mixer-meter');
 
     // Add data value to div tag
-    $(this).attr('data-vol', 0);
+    $(this)
+        .attr('data-vol', 0);
 
     // Add LEDs to meter
     var LEDHTML = '<ul>';
-    for(var i = 0; i < numberOfLEDs; i++) {
+    for (var i = 0; i < numberOfLEDs; i++) {
         // Add colors to LEDs
-        if(i <= 1)
+        if (i <= 1)
             LEDHTML += '<li class="mixer-led mixer-led-red"></li>';
-        else if(i <= 3)
+        else if (i <= 3)
             LEDHTML += '<li class="mixer-led mixer-led-yellow"></li>';
         else
             LEDHTML += '<li class="mixer-led mixer-led-green"></li>';
     }
     LEDHTML += '</ul>';
-    $(this).html(LEDHTML);
+    $(this)
+        .html(LEDHTML);
 
     // Check if data-vol has changed
-    $(this).on('volumeChange', function() {
-        // Get <ul> from div
-        var ul = $(this).children(0);
+    $(this)
+        .on('volumeChange', function () {
+            // Get <ul> from div
+            var ul = $(this)
+                .children(0);
 
-        // Get data-vol value
-        var volume = parseInt($(this).attr('data-vol'));
+            // Get data-vol value
+            var volume = parseInt($(this)
+                .attr('data-vol'));
 
-        // How many LEDs to light up
-        var lightUp = Math.ceil((volume / 100) * numberOfLEDs);
+            // How many LEDs to light up
+            var lightUp = Math.ceil((volume / 100) * numberOfLEDs);
 
-        // Find elements you need to light up
-        var onLEDs = ((numberOfLEDs - lightUp) < 0) ? ul.find('li:gt(' + (numberOfLEDs - lightUp) + ')') : ul.children();
-        var offLEDs = ul.find('li:lt(' + (numberOfLEDs - lightUp) + ')');
+            // Find elements you need to light up
+            var onLEDs = ((numberOfLEDs - lightUp) < 0) ? ul.find('li:gt(' + (numberOfLEDs - lightUp) + ')') : ul.children();
+            var offLEDs = ul.find('li:lt(' + (numberOfLEDs - lightUp) + ')');
 
-        // Change color of LEDS that are on
-        onLEDs.each(function(index, val) {
-            if($(val).hasClass('mixer-led-red'))
-                $(val).css('background-color', 'rgb(255, 0, 0)');
-            else if($(val).hasClass('mixer-led-yellow'))
-                $(val).css('background-color', 'rgb(255, 255, 0)');
-            else if($(val).hasClass('mixer-led-green'))
-                $(val).css('background-color', 'rgb(0, 255, 0)');
+            // Change color of LEDS that are on
+            onLEDs.each(function (index, val) {
+                if ($(val)
+                    .hasClass('mixer-led-red'))
+                    $(val)
+                    .css('background-color', 'rgb(255, 0, 0)');
+                else if ($(val)
+                    .hasClass('mixer-led-yellow'))
+                    $(val)
+                    .css('background-color', 'rgb(255, 255, 0)');
+                else if ($(val)
+                    .hasClass('mixer-led-green'))
+                    $(val)
+                    .css('background-color', 'rgb(0, 255, 0)');
+            });
+
+            // Change color of LEDS that are off
+            offLEDs.each(function (index, val) {
+                if ($(val)
+                    .hasClass('mixer-led-red'))
+                    $(val)
+                    .css('background-color', 'rgb(100, 0, 0)');
+                else if ($(val)
+                    .hasClass('mixer-led-yellow'))
+                    $(val)
+                    .css('background-color', 'rgb(100, 100, 0)');
+                else if ($(val)
+                    .hasClass('mixer-led-green'))
+                    $(val)
+                    .css('background-color', 'rgb(0, 100, 0)');
+            });
+
+            // Set the clipping LED to stay on for a second longer
+            if (lightUp == numberOfLEDs) {
+                clipping = true;
+                setTimeout(function () {
+                    clipping = false;
+                }, 1000);
+            }
+            if (clipping == true) {
+                ul.find('li:first')
+                    .css('background-color', 'rgb(255, 0, 0)');
+            }
         });
-
-        // Change color of LEDS that are off
-        offLEDs.each(function(index, val) {
-            if($(val).hasClass('mixer-led-red'))
-                $(val).css('background-color', 'rgb(100, 0, 0)');
-            else if($(val).hasClass('mixer-led-yellow'))
-                $(val).css('background-color', 'rgb(100, 100, 0)');
-            else if($(val).hasClass('mixer-led-green'))
-                $(val).css('background-color', 'rgb(0, 100, 0)');
-        });
-
-        // Set the clipping LED to stay on for a second longer
-        if(lightUp == numberOfLEDs) {
-            clipping = true;
-            setTimeout(function() {
-                clipping = false;
-            }, 1000);
-        }
-        if(clipping == true) {
-            ul.find('li:first').css('background-color', 'rgb(255, 0, 0)');
-        }
-    });
 };
 
 // Change volume
-$.fn.MeterVolume = function(vol) {
+$.fn.MeterVolume = function (vol) {
     // Find out if you have the data-vol data
-    var attr = $(this).attr('data-vol');
+    var attr = $(this)
+        .attr('data-vol');
 
     // Check if you have the attr or not
     if (typeof attr !== typeof undefined && attr !== false) {
         // Set new value for data-vol and trigger event
-        $(this).attr('data-vol', vol).trigger('volumeChange');
+        $(this)
+            .attr('data-vol', vol)
+            .trigger('volumeChange');
     }
 }
